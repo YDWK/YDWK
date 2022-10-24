f2dbc5f1ddca52c9e1286870fd4491b0b47c6013
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-10-24 21:22:56 +0100

    added snowflake format structure (#43)

--- src/main/kotlin/io/github/ydwk/ydwk/util/GetterSnowFlake.kt
+++ src/main/kotlin/io/github/ydwk/ydwk/util/GetterSnowFlake.kt
@@ -18,11 +18,60 @@
  */ 
 package io.github.ydwk.ydwk.util
 
+/**
+ * A snowflake is a 64-bit integer that is used to uniquely identify a Discord entity.
+ *
+ * @see [DiscordDocs](https://discord.com/developers/docs/reference#snowflakes)
+ */
 interface GetterSnowFlake {
+
+    /**
+     * Gets the id of this snowflake as a Long.
+     *
+     * @return the id of this snowflake as a [Long]
+     */
     val asLong: Long
 
+    /**
+     * Gets the id of this snowflake as a String.
+     *
+     * @return the id of this snowflake as a [String]
+     */
     val asString: String
         get() = asLong.toString()
+
+    /**
+     * Gets the timestamp of this snowflake. (Milliseconds since Discord Epoch, the first second of
+     * 2015 or 1420070400000.)
+     *
+     * @return the timestamp of this snowflake.
+     */
+    val asTimestamp: Long
+        get() = (asLong shr 22) + 1420070400000
+
+    /**
+     * Gets the worker id of this snowflake.
+     *
+     * @return the worker id of this snowflake.
+     */
+    val asWorkerId: Long
+        get() = (asLong and 0x3E0000) shr 17
+
+    /**
+     * Gets the process id of this snowflake.
+     *
+     * @return the process id of this snowflake.
+     */
+    val asProcessId: Long
+        get() = (asLong and 0x1F000) shr 12
+
+    /**
+     * Gets the increment of this snowflake.
+     *
+     * @return the increment of this snowflake.
+     */
+    val asIncrement: Long
+        get() = asLong and 0xFFF
 
     companion object {
         val asNull: GetterSnowFlake? = null
