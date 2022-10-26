4527934805ffbbb7d119a5d5040b8eaa9d288fca
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-10-26 13:30:53 +0100

    finished
    need to update docs

--- src/main/kotlin/io/github/ydwk/ydwk/BotBuilder.kt
+++ src/main/kotlin/io/github/ydwk/ydwk/BotBuilder.kt
@@ -18,16 +18,94 @@
  */ 
 package io.github.ydwk.ydwk
 
+import io.github.ydwk.ydwk.cache.CacheIds
+import io.github.ydwk.ydwk.impl.YDWKImpl
+import io.github.ydwk.ydwk.ws.util.GateWayIntent
+import okhttp3.OkHttpClient
+
 object BotBuilder {
-    var token: String? = null
+    private var token: String? = null
+    private var okHttpClient: OkHttpClient? = null
+    private var intents: MutableList<GateWayIntent> = mutableListOf()
+    private var allowedCache: MutableList<CacheIds> = mutableListOf()
+    private var disallowedCache: MutableList<CacheIds> = mutableListOf()
+    private var guildIds = mutableListOf<String>()
 
     fun createDefaultBot(token: String): BotBuilder {
         this.token = token
+        intents.addAll(GateWayIntent.getDefaultIntents())
+        allowedCache.addAll(CacheIds.getDefaultCache())
         return this
     }
 
     fun createDefaultBot(token: Long): BotBuilder {
-        this.token = token.toString()
+        return createDefaultBot(token.toString())
+    }
+
+    fun createCustomBot(token: String, intents: MutableList<GateWayIntent>): BotBuilder {
+        this.token = token
+        this.intents.addAll(intents)
         return this
+    }
+
+    fun createCustomBot(token: Long, intents: MutableList<GateWayIntent>): BotBuilder {
+        return createCustomBot(token.toString(), intents)
+    }
+
+    fun setOkHttpClient(okHttpClient: OkHttpClient): BotBuilder {
+        this.okHttpClient = okHttpClient
+        return this
+    }
+
+    fun setIntents(intents: MutableList<GateWayIntent>, clearIntent: Boolean): BotBuilder {
+        if (clearIntent) {
+            this.intents.clear()
+        }
+        this.intents.addAll(intents)
+        return this
+    }
+
+    fun setIntents(intents: MutableList<GateWayIntent>): BotBuilder {
+        return setIntents(intents, true)
+    }
+
+    fun setAllowedCache(allowedCache: MutableList<CacheIds>, clearCache: Boolean): BotBuilder {
+        if (clearCache) {
+            this.allowedCache.clear()
+        }
+        this.allowedCache.addAll(allowedCache)
+        return this
+    }
+
+    fun setAllowedCache(allowedCache: MutableList<CacheIds>): BotBuilder {
+        return setAllowedCache(allowedCache, false)
+    }
+
+    fun setDisallowedCache(
+        disallowedCache: MutableList<CacheIds>,
+        clearCache: Boolean
+    ): BotBuilder {
+        if (clearCache) {
+            this.disallowedCache.clear()
+        }
+        this.disallowedCache.addAll(disallowedCache)
+        return this
+    }
+
+    fun setDisallowedCache(disallowedCache: MutableList<CacheIds>): BotBuilder {
+        return setDisallowedCache(disallowedCache, false)
+    }
+
+    fun setGuildIds(guildIds: MutableList<String>): BotBuilder {
+        this.guildIds.addAll(guildIds)
+        return this
+    }
+
+    fun build(): YDWK {
+        val ydwk = YDWKImpl(okHttpClient ?: OkHttpClient())
+        ydwk.setWebSocketManager(token!!, intents)
+        ydwk.setAllowedCache(allowedCache)
+        ydwk.setDisallowedCache(disallowedCache)
+        return ydwk
     }
 }
