40af9bd28e1e2fafd1710d76cf258611683755e2
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-12-30 22:33:25 +0000

    Added support for forum channels

---
+++ src/main/kotlin/io/github/ydwk/ydwk/entities/channel/guild/forum/ForumTag.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 YDWK inc.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package io.github.ydwk.ydwk.entities.channel.guild.forum
+
+import io.github.ydwk.ydwk.entities.util.GenericEntity
+import io.github.ydwk.ydwk.util.GetterSnowFlake
+import io.github.ydwk.ydwk.util.NameAbleEntity
+import io.github.ydwk.ydwk.util.SnowFlake
+
+interface ForumTag : SnowFlake, GenericEntity, NameAbleEntity {
+
+    /**
+     * Whether this tag can only be added to or removed from threads by a member with the
+     * MANAGE_THREADS permission.
+     *
+     * @return whether this tag can only be added to or removed from threads by a member with the
+     *   MANAGE_THREADS permission.
+     */
+    val isManaged: Boolean
+
+    /**
+     * The emoji id of this tag.
+     *
+     * @return the emoji id of this tag.
+     */
+    val emojiId: GetterSnowFlake?
+
+    /**
+     * The emoji name of this tag.
+     *
+     * @return the emoji name of this tag.
+     */
+    val emojiName: String?
+}
---
+++ src/main/kotlin/io/github/ydwk/ydwk/impl/entities/channel/guild/forum/ForumTagImpl.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 YDWK inc.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package io.github.ydwk.ydwk.impl.entities.channel.guild.forum
+
+import com.fasterxml.jackson.databind.JsonNode
+import io.github.ydwk.ydwk.YDWK
+import io.github.ydwk.ydwk.entities.channel.guild.forum.ForumTag
+import io.github.ydwk.ydwk.util.EntityToStringBuilder
+import io.github.ydwk.ydwk.util.GetterSnowFlake
+
+class ForumTagImpl(
+    override val ydwk: YDWK,
+    override val json: JsonNode,
+    override val idAsLong: Long
+) : ForumTag {
+    override val isManaged: Boolean
+        get() = json["moderated"].asBoolean()
+
+    override val emojiId: GetterSnowFlake?
+        get() = if (json.has("emoji_id")) GetterSnowFlake.of(json["emoji_id"].asText()) else null
+    override val emojiName: String?
+        get() = if (json.has("name")) json["name"].asText() else null
+
+    override var name: String = json["name"].asText()
+
+    override fun toString(): String {
+        return EntityToStringBuilder(ydwk, this).toString()
+    }
+}
