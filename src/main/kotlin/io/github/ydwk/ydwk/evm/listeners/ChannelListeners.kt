67f5d294bd42c1ff19d1c9c8e3ef1af7ff503cfd
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-02-17 10:18:09 +0000

    added onChannelCreate method

--- src/main/kotlin/io/github/ydwk/ydwk/evm/listeners/ChannelListeners.kt
+++ src/main/kotlin/io/github/ydwk/ydwk/evm/listeners/ChannelListeners.kt
@@ -1,4 +1,12 @@
 package io.github.ydwk.ydwk.evm.listeners
 
+import io.github.ydwk.ydwk.evm.event.events.channel.ChannelCreateEvent
+
 interface ChannelListeners {
+    /**
+     * Listens to ChannelCreateEvent
+     *
+     * @param event The ChannelCreateEvent
+     */
+    fun onChannelCreate(event: ChannelCreateEvent) {}
 }
