ca8cfef3a38a60949c062e194ba7e26c3294ec90
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-02-17 10:33:34 +0000

    added InteractionListeners

--- src/main/kotlin/io/github/ydwk/ydwk/evm/event/events/interaction/message/MessageCommandEvent.kt
+++ src/main/kotlin/io/github/ydwk/ydwk/evm/event/events/interaction/message/MessageCommandEvent.kt
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */ 
-package io.github.ydwk.ydwk.evm.event.events.interaction.slash
+package io.github.ydwk.ydwk.evm.event.events.interaction.message
 
 import io.github.ydwk.ydwk.YDWK
 import io.github.ydwk.ydwk.evm.event.Event
