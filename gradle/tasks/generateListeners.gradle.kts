50aff3379fe2221ee2262edd5fb4f236a83b1078
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-04-20 10:02:09 +0100

    Update generateListeners.gradle.kts

--- gradle/tasks/generateListeners.gradle.kts
+++ gradle/tasks/generateListeners.gradle.kts
@@ -25,42 +25,42 @@ tasks.register("generateListeners") {
         val interactionListeners = classGraph.getClassesImplementing("io.github.ydwk.ydwk.evm.listeners.extendable.ExtendableInteractionListener")
         val guildModerationListeners = classGraph.getClassesImplementing("io.github.ydwk.ydwk.evm.listeners.extendable.ExtendableGuildModerationListener")
 
-        val coreListenerFile = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "CoreListeners").addType(
-            TypeSpec.Builder.interfaceBuilder("CoreListeners")
+        val coreListenerFile : TypeSpec = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "CoreListeners").addType(
+            TypeSpec.interfaceBuilder("CoreListeners")
                 .addSuperinterface(ClassName("io.github.ydwk.ydwk.evm.backend.event.IEventListener", "IEventListener"))
         )
                 
-        val guildListenerFile = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "GuildListeners")
+        val guildListenerFile : TypeSpec = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "GuildListeners")
             .addType(
                 TypeSpec.interfaceBuilder("GuildListeners")
                     .addSuperinterface(ClassName("io.github.ydwk.ydwk.evm.backend.event.IEventListener", "IEventListener"))
             )
             
-        val channelListenerFile = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "ChannelListeners")
+        val channelListenerFile : TypeSpec = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "ChannelListeners")
             .addType(
                 TypeSpec.interfaceBuilder("ChannelListeners")
                     .addSuperinterface(ClassName("io.github.ydwk.ydwk.evm.backend.event.IEventListener", "IEventListener"))
             )
 
-        val userListenerFile = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "UserListeners")
+        val userListenerFile : TypeSpec = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "UserListeners")
             .addType(
                 TypeSpec.interfaceBuilder("UserListeners")
                     .addSuperinterface(ClassName("io.github.ydwk.ydwk.evm.backend.event.IEventListener", "IEventListener"))
             )
             
-        val voiceListenerFile = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "VoiceListeners")
+        val voiceListenerFile : TypeSpec = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "VoiceListeners")
             .addType(
                 TypeSpec.interfaceBuilder("VoiceListeners")
                     .addSuperinterface(ClassName("io.github.ydwk.ydwk.evm.backend.event.IEventListener", "IEventListener"))
             )
             
-        val interactionListenerFile = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "InteractionListeners")
+        val interactionListenerFile : TypeSpec = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "InteractionListeners")
             .addType(
                 TypeSpec.interfaceBuilder("InteractionListeners")
                     .addSuperinterface(ClassName("io.github.ydwk.ydwk.evm.backend.event.IEventListener", "IEventListener"))
             )
             
-        val guildModerationListenerFile = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "GuildModerationListeners")
+        val guildModerationListenerFile : TypeSpec = FileSpec.builder("io.github.ydwk.ydwk.evm.listeners", "GuildModerationListeners")
             .addType(
                 TypeSpec.interfaceBuilder("GuildModerationListeners")
                     .addSuperinterface(ClassName("io.github.ydwk.ydwk.evm.backend.event.IEventListener", "IEventListener"))
